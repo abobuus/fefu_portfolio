8e653b4124235eb3ca8cf8967c4ca7e749327521
Muzychenko Alevtina <fghbaim808@gmail.com>
2023-12-17 22:37:37 +1000

    Create registration

--- fefu_portfolio/users/forms.py
+++ fefu_portfolio/users/forms.py
@@ -1,8 +1,13 @@
 from django.contrib.auth import admin, get_user_model
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django.forms import forms
 
 
 class RegisterForm(UserCreationForm):
-    ...
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password2'].required = False
--- fefu_portfolio/users/models.py
+++ fefu_portfolio/users/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin, AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    ...
+    pass
